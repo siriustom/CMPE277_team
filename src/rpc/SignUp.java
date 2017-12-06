8e8e92416220c756caada82413bcc44aeb8cb02c
siriustom <linxiaoran0210@gmail.com>
2017-12-05 23:58:34 -0800

    sign up servlet editing

--- src/rpc/SignUp.java
+++ src/rpc/SignUp.java
@@ -7,12 +7,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+
+import db.MongoDBUtil;
+
 /**
  * Servlet handles sign up process after sign up request
  */
 @WebServlet("/SignUp")
 public class SignUp extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+	
+	private static final MongoClient mongoClient = new MongoClient("54.200.143.121", 27017);
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -27,6 +36,21 @@ public class SignUp extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		try {
+			JSONObject msg = new JSONObject();
+			// get request parameters for userID and password
+			String universityId = request.getParameter("university_id");
+			String email = request.getParameter("email");
+			String password = request.getParameter("password");
+			String verifyCode = request.getParameter("verification_code");
+			MongoDatabase db = mongoClient.getDatabase(MongoDBUtil.DB_NAME);
+			String message = "user has been signed up";
+			msg.put("status", "OK");
+			msg.put("msg", message);
+			RpcHelper.writeJsonObject(response, msg);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
