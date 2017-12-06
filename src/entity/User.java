e39e5a439ab65049364d244b228483630526ceec
SaxonWang99 <shihan.wang@sjsu.edu>
2017-12-05 22:06:58 -0800

    add attributes

--- src/entity/User.java
+++ src/entity/User.java
@@ -1,5 +1,41 @@
 package entity;
 
 public class User {
-	
+
+    private String universityid;
+    private String email;
+    private String password;
+    private BookCopy[] copies;
+
+    public String getUniversityid() {
+        return universityid;
+    }
+
+    public void setUniversityid(String universityid) {
+        this.universityid = universityid;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public BookCopy[] getCopies() {
+        return copies;
+    }
+
+    public void setCopies(BookCopy[] copies) {
+        this.copies = copies;
+    }
 }
