48edc9c6f2d06a1a28c511f6736d9b83733fcf2b
siriustom <linxiaoran0210@gmail.com>
2017-12-05 22:16:03 -0800

    4th

--- src/rpc/EditBookCatalog.java
+++ src/rpc/EditBookCatalog.java
@@ -8,34 +8,25 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class SearchBook
+ * Servlet implementation class EditBookCataLog
  */
-@WebServlet("/search")
-public class SearchBook extends HttpServlet {
+@WebServlet("/EditBookCataLog")
+public class EditBookCatalog extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public SearchBook() {
+    public EditBookCatalog() {
         super();
         // TODO Auto-generated constructor stub
     }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
 	}
 
 }
