70894f52676c3bd8b5677afc2e1fbb1af915e3b5
siriustom <linxiaoran0210@gmail.com>
2017-12-07 02:16:20 -0800

    debug

--- src/rpc/RequestSignUp.java
+++ src/rpc/RequestSignUp.java
@@ -47,6 +47,7 @@ public class RequestSignUp extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		response.setContentType("application/json");
 		try {
 			JSONObject msg = new JSONObject();
 
@@ -58,23 +59,14 @@ public class RequestSignUp extends HttpServlet {
 
 			// communicate to db
 			List<String> listOfEmail = db.queryByEmail(email);
-			int atSignStore = listOfEmail.get(0).indexOf("@");
 			if (listOfEmail.size() >= 2) {
 				String moreThanTwo = "you cannot have more than two accounts.";
 				msg.put("status", "error");
 				msg.put("msg", moreThanTwo);
 				RpcHelper.writeJsonObject(response, msg);
-			} else if (listOfEmail.size() == 1
-					&& (email.substring(atSign + 1) == "sjsu" && listOfEmail.get(0).substring(atSignStore + 1) == "sjsu"
-							|| email.substring(atSign + 1) != "sjsu"
-									&& listOfEmail.get(0).substring(atSignStore + 1) != "sjsu")) {
-				String moreThanOne = "you cannot have more than one account in certain email domain.";
-				msg.put("status", "error");
-				msg.put("msg", moreThanOne);
-				RpcHelper.writeJsonObject(response, msg);
-			} else {
+			} else if (listOfEmail.size() == 0) {
 				// generate a random number for verification code
-				int code = codeGenerate();
+				String code = codeGenerate();
 
 				// no need to verify duplicate email, just send back verification code
 				String verifyCode = "verification code sent";
@@ -82,34 +74,61 @@ public class RequestSignUp extends HttpServlet {
 				msg.put("verification_code", code);
 				msg.put("msg", verifyCode);
 				RpcHelper.writeJsonObject(response, msg);
-
-				// send email
-				String vcode = String.valueOf(code);
-				String from = "zeningdeng@gmail.com";
-				String host = "server";
-				Properties properties = System.getProperties();
-				properties.setProperty("mail.smtp.host", host);
-				properties.setProperty("mail.user", "zeningdeng2@gmail.com");
-				properties.setProperty("mail.password", "zdpassword");
-				Session session = Session.getDefaultInstance(properties);
-				try {
-					MimeMessage e = new MimeMessage(session);
-					e.setFrom(new InternetAddress(from));
-					e.addRecipient(Message.RecipientType.TO, new InternetAddress(email));
-					e.setSubject("This is validation code!");
-					e.setText("Your code is " + vcode);
-					Transport.send(e);
-				} catch (MessagingException mex) {
-					mex.printStackTrace();
+				
+				//send email
+				sendEmail(code, email);
+			} else {
+				int atSignStore = listOfEmail.get(0).indexOf("@");
+				if (email.substring(atSign + 1) == "sjsu.edu" && listOfEmail.get(0).substring(atSignStore + 1) == "sjsu.edu"
+						|| email.substring(atSign + 1) != "sjsu.edu"
+								&& listOfEmail.get(0).substring(atSignStore + 1) != "sjsu.edu") {
+					String moreThanOne = "you cannot have more than one account in certain email domain.";
+					msg.put("status", "error");
+					msg.put("msg", moreThanOne);
+					RpcHelper.writeJsonObject(response, msg);
+				} else {
+					
+					// generate a random number for verification code
+					String code = codeGenerate();
+					
+					// no need to verify duplicate email, just send back verification code
+					String verifyCode = "verification code sent";
+					msg.put("status", "OK");
+					msg.put("verification_code", code);
+					msg.put("msg", verifyCode);
+					RpcHelper.writeJsonObject(response, msg);
+					
+					//send email
+					sendEmail(code, email);
 				}
-			}
+			} 
 		} catch (JSONException e) {
 			e.printStackTrace();
 		}
 	}
 
-	private int codeGenerate() {
-		return (int) (Math.random() * 1000000 + 1);
+	private String codeGenerate() {
+		return String.valueOf((int) (Math.random() * 1000000 + 1));
 	}
-
+	
+	private void sendEmail(String code, String to) {
+		// send email
+		String from = "zeningdeng@gmail.com";
+		String host = "server";
+		Properties properties = System.getProperties();
+		properties.setProperty("mail.smtp.host", host);
+		properties.setProperty("mail.user", "zeningdeng2@gmail.com");
+		properties.setProperty("mail.password", "zdpassword");
+		Session session = Session.getDefaultInstance(properties);
+		try {
+			MimeMessage e = new MimeMessage(session);
+			e.setFrom(new InternetAddress(from));
+			e.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+			e.setSubject("This is validation code!");
+			e.setText("Your code is " + code);
+			Transport.send(e);
+		} catch (MessagingException mex) {
+			mex.printStackTrace();
+		}
+	}
 }
